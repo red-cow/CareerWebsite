2f4a044c978fe10adcbaacf658545ef444d32666
Ryan_Sack <rmsack22@gmail.com>
2025-05-29 20:13:08 -0400

    Update 4
    added all json. touched up navigation and added job history

--- js/app.js
+++ js/app.js
@@ -9,15 +9,38 @@ window.onload = () => {
     .then(data => {
       data.forEach(job => {
         const divWorkCard = document.createElement('div');
-        divWorkCard.classList.add('Work-card');
+        divWorkCard.classList.add('work-card');
         
         const h3Jobtitle = document.createElement('h3')
         h3Jobtitle.classList.add("job-title")
         h3Jobtitle.textContent = job.title
-        console.log(job.title)
+
+        const pPosition = document.createElement('p')
+        pPosition.classList.add("position")
+        pPosition.textContent = job.position
+
+        const pLocationTime = document.createElement('p')
+        pLocationTime.classList.add("location-time")
+        pLocationTime.textContent = `${job.location} • ${job.time}`
+
+        const ulSkills = document.createElement('ul')
+        ulSkills.classList.add("skills")
+        console.log(job.skills)
+        job.skills.forEach(element => {
+          const liSkillTemp = document.createElement('li')
+          liSkillTemp.textContent = element
+          ulSkills.appendChild(liSkillTemp)
+        });
 
         divWorkCard.appendChild(h3Jobtitle)
-        document.getElementById('experience-container').appendChild(divWorkCard);
+        divWorkCard.appendChild(pPosition)
+        divWorkCard.appendChild(pLocationTime)
+        divWorkCard.appendChild(ulSkills)
+
+
+        const container = document.getElementById('experience-container')
+        
+        container.appendChild(divWorkCard);
       });
     })
     .catch(error => {
